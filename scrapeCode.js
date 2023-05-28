fc3d521990ab3fe795b7c0c79affeaa36028ba82
Htet Yar Zar <43663903+Hashyz@users.noreply.github.com>
2023-05-28 20:27:51 +0630

    Update scrapeCode.js

--- scrapeCode.js
+++ scrapeCode.js
@@ -1,3 +1,4 @@
+// Here You can type your custom JavaScript...
 //just paste in web telegram console
 pastCode = "";
 // Function to process the last message
@@ -24,14 +25,25 @@ function processLastMessage(code) {
 // Function to handle new messages
 function handleNewMessage(event) {
     const message = event.currentTarget;//event.target;
-    let l = message.getElementsByClassName("message-content-wrapper can-select-text");
+    /*let l = message.getElementsByClassName("message-content-wrapper can-select-text");
     let lastPosi = l[l.length - 1];
     const code = lastPosi.querySelector("div").getElementsByClassName("content-inner")[0].querySelector("code").textContent;
-    if (pastCode != code){
-        pastCode = code;
-    processLastMessage(code);
-    }
+*/
+    let l = message.getElementsByClassName("message-content-wrapper can-select-text");
+    let lastPosi = l[l.length-1];let codeEle = lastPosi.querySelector("div").getElementsByClassName("content-inner")[0];
+    if (lastPosi.querySelector("code") == null){
+    code = codeEle.querySelectorAll("div")[1].firstChild
+}else{
+    code = lastPosi.querySelector("code").textContent;
 }
-
+if (code.length == 8){
+    //console.log(code);
+if (pastCode != code){
+    pastCode = code;
+    processLastMessage(code);
+    }    
+    }
+    
+}
 // Listen for new message events
 document.addEventListener("DOMNodeInserted", handleNewMessage);
