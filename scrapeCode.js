53fd04ff28f25474c8c936dadb6112db5e0ddb53
Htet Yar Zar <43663903+Hashyz@users.noreply.github.com>
2023-05-28 18:44:35 +0630

    Create scrapeCode.js

---
+++ scrapeCode.js
@@ -0,0 +1,37 @@
+//just paste in web telegram console
+pastCode = "";
+// Function to process the last message
+function processLastMessage(code) {
+
+  // Send the code to your server
+  fetch(`http://localhost/?code=${code}`)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not OK');
+      }
+      return response.json();
+    })
+    .then(data => {
+      // Process the response data from the server
+      console.log(data);
+    })
+    .catch(error => {
+      // Handle the error
+      console.error('Error:', error);
+    });
+}
+
+// Function to handle new messages
+function handleNewMessage(event) {
+    const message = event.currentTarget;//event.target;
+    let l = message.getElementsByClassName("message-content-wrapper can-select-text");
+    let lastPosi = l[l.length - 1];
+    const code = lastPosi.querySelector("div").getElementsByClassName("content-inner")[0].querySelector("code").textContent;
+    if (pastCode != code){
+        pastCode = code;
+    processLastMessage(code);
+    }
+}
+
+// Listen for new message events
+document.addEventListener("DOMNodeInserted", handleNewMessage);
